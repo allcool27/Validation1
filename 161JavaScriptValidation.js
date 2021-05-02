e2687a6a29c3c4da7560d1906a78ff3733adebea
allcool27 <itsallcool27@gmail.com>
2021-05-02 14:11:59 -0700

    Add files via upload

---
+++ 161JavaScriptValidation.js
@@ -0,0 +1,21 @@
+function formValidation()
+{
+var uname = document.registration.username;
+var uemail = document.registration.email;
+var uemail = document.registration.phone;
+
+if(allLetter(uname))
+{
+if(alphanumeric(uadd))
+{ 
+if(ValidateEmail(uemail))
+{
+}
+}
+} 
+}
+}
+}
+}
+return false;
+}
